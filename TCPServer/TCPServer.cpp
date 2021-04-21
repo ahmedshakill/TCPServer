31ed8c984a99598f9ec2afed7e9f12b1ee524098
kingpin <shakil.000024@gmail.com>
2021-04-21 17:37:59 +0600

    man man man!!!  client closing  connection doesn't make server break down .

--- TCPServer/TCPServer.cpp
+++ TCPServer/TCPServer.cpp
@@ -9,6 +9,7 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast/ssl.hpp>
 #include <boost/beast/version.hpp>
+#include <boost/exception/all.hpp>
 #include <boost/filesystem.hpp>
 #include "server_certificate.hpp"
 
@@ -22,7 +23,7 @@ using error_code = boost::system::error_code;
 
 //////////////////////////////
 
-#define check(msg) if(ec){std::cout<<msg;return;}
+#define check(msg) if(ec){std::cout<<msg;}
 
 //////////////////////////////
 
@@ -43,60 +44,95 @@ public:
 			});
 		/*read_client();*/
 	}
+	~session() { 
+		beast::close_socket(beast::get_lowest_layer(stream_));
+		//stream_.async_shutdown([](error_code ec) 
+		//	{
+		//		if (ec != boost::asio::ssl::error::stream_truncated)
+		//		{
+		//			std::cout << ec.message() << std::endl;
+		//			/*throw boost::system::system_error{ ec };*/
+		//		}
 
+		//	});
+	};
 private:
 
 	void perform_ssl_handshake()
 	{
-		beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+		//beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
 		stream_.async_handshake(ssl::stream_base::server, 
-			[self=shared_from_this()](error_code ec) 
-			{
-			if (ec)
-				std::cout << "error handshake\n";
-					/*self->stream_.shutdown();*/
-				self->read_client();
+				[self=shared_from_this()](error_code ec) 
+				{
+				self->ec = ec;
+				if (ec) {
+					std::cout << "error handshake\n";
+				}
+				else {
+					self->read_client();
+				}
 			});
 
 	}
 
 	void read_client()
 	{
-		std::cout << "Reading Client\n";
-		http::async_read(stream_,streambuf_,request_, [self=shared_from_this()](error_code ec,std::size_t bytes_transferred) {
-				std::cout <<&self->streambuf_;
-				self->streambuf_.consume(bytes_transferred);
-				self->write_client();
-				//self->read_client();
-			});
-		//read_client();
+		try{
+			std::cout << "Reading Client\n";
+			request_ = {};
+			http::async_read(stream_,streambuf_,request_, [self=shared_from_this()](error_code ec,std::size_t bytes_transferred) {
+					self->ec = ec;
+					std::cout << self->request_<<" "<<bytes_transferred;
+					self->write_client();
+				});
+		}
+		catch (const boost::system::system_error& ec)
+		{
+			std::cout << "\n\nstart_accept: " << ec.what();
+			if (ec.code() == boost::asio::error::connection_aborted)
+			{
+				
+			}
+		}
 	}
-
-	//io::buffer("HTTP/1.1 200 OK\r\nServer: Beast\r\nContent - Length: 38 \r\n\r\n <html><body>hello world</body></html>")
 
 	void write_client()
 	{
-		std::cout << "\n\nwriting\n\n";
-		http::string_body::value_type body;
-		body.append("<html><body>hello world</body></html>");
-		auto const size = body.size();
-		http::response<http::string_body> res_{
-		std::piecewise_construct,
-		std::make_tuple(std::move(body)),
-		std::make_tuple(http::status::ok, request_.version()) };
-		res_.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-		res_.set(http::field::content_type, "text / html");
-		res_.content_length(size);
-		res_.keep_alive(request_.keep_alive());
-		http::async_write(stream_, res_, [self = shared_from_this()](error_code ec,std::size_t bytes_transferred)
+		try {
+			std::cout << "\n\nwriting\n\n";
+			http::string_body::value_type body;
+			body.append("<html><body>hello world</body></html>");
+			auto const size = body.size();
+			http::response<http::string_body> res_{
+			std::piecewise_construct,
+			std::make_tuple(std::move(body)),
+			std::make_tuple(http::status::ok, request_.version()) };
+			res_.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+			res_.set(http::field::content_type, "text / html");
+			res_.content_length(size);
+			res_.keep_alive(request_.keep_alive());
+			/*http::async_write(stream_, res_, [self = shared_from_this()](error_code ec,std::size_t bytes_transferred)
+			{
+				self->read_client();
+			});*/
+		
+			http::write(stream_, res_);
+			read_client();
+		}
+		catch (const boost::system::system_error& ec)
 		{
-			self->read_client();
-
-		});
-
+			std::cout << "\n\nstart_accept: " << ec.what();
+		}
+		catch (const std::exception& e)
+		{
+			
+			std::cout << "\n\n sfskladf " << e.what();
+		}
 	}
+	
 private:
 	beast::flat_buffer streambuf_;
+	error_code ec;
 	beast::ssl_stream<beast::tcp_stream>stream_;
 	http::request<http::string_body> request_;
 };
@@ -104,29 +140,17 @@ private:
 class server :public  std::enable_shared_from_this<server>
 {
 public:
-
-	
-
 	server(io::io_context& io_context)
 		: io_context_(io_context),
-		acceptor_(io_context)
+		acceptor_(io_context, tcp::endpoint{ io::ip::make_address("127.0.0.1"),8080 })
 	{
 		error_code ec;
-		acceptor_.open(endpoint_.protocol(),ec);
-		check("error openning\n");
-
-		acceptor_.set_option(io::socket_base::reuse_address(true), ec);
-		check( "error set_option\n")
-
-		acceptor_.bind(endpoint_, ec);
-		check( "error binding\n")
-
 		acceptor_.listen(io::socket_base::max_listen_connections, ec);
 		check("error listenning\n")
+
 	}
 	void start()
 	{
-
 		load_server_certificate(ctx_);
 		start_accept();
 	}
@@ -134,16 +158,22 @@ public:
 	void start_accept()
 	{
 		// The new connection gets its own strand
+		//socket.emplace(io::make_strand(io_context_));
+		//std::shared_ptr<tcp::socket> pSocket(new tcp::socket(io::make_strand(io_context_)));
 		acceptor_.async_accept(
-			io::make_strand(io_context_), [self=shared_from_this()](error_code ec, tcp::socket socket) {
+			[self=shared_from_this()](error_code ec,tcp::socket&& pSocket) {
 			check("error accepting connection\n")
-				std::cout << "\nnew connection\n";
-			/*self->v.emplace_back([&]() {self->io_context_.run(); });*/
+			std::cout << "\nnew connection\n";
 			
-			auto client = std::make_shared<session>(self->ctx_,std::move(socket));
-			client->serve_client();
-			
-			self->start_accept();	
+			try
+			{
+				 std::make_shared<session>(self->ctx_, std::move(pSocket))->serve_client(); 
+				self->start_accept();
+			}
+			catch (const error_code &ec)
+			{
+				std::cout << "\n\nstart_accept: " << ec.message();
+			}		
 		});
 	}
 
@@ -152,13 +182,21 @@ private:
 	ssl::context ctx_{ ssl::context::tlsv13 };
 	tcp::acceptor acceptor_;
 	tcp::endpoint endpoint_{ io::ip::make_address("127.0.0.1"),8080 };
-	
+	std::optional<tcp::socket> socket;
 };
 
 int main(int argc, char* argv[])
 {
-	io::io_context io_context;
-	std::make_shared<server>(io_context)->start();
-	io_context.run();
-	return 0;
+	try
+	{
+		io::io_context io_context;
+		std::make_shared<server>(io_context)->start();
+		io_context.run();
+		return 0;
+	}
+	catch (const error_code& e)
+	{
+		std::cout<<"what : " << e.message();
+	}
+	
 }
