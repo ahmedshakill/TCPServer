c5b71249938531617995e97e5fb753d41bc09371
kingpin <shakil.000024@gmail.com>
2021-04-28 10:14:51 +0600

    pushing VS Version

--- TCPServer/TCPServer.cpp
+++ TCPServer/TCPServer.cpp
@@ -1,5 +1,5 @@
-// #define _WIN32_WINNT 0xA000006
-// #define _CRT_SECURE_NO_WARNINGS
+#define _WIN32_WINNT 0xA000006
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
 #include <optional>
@@ -33,8 +33,8 @@ using error_code = boost::system::error_code;
 class session : public std::enable_shared_from_this<session>
 {
 public:
-	session(tcp::socket&& socket) :
-		stream_(std::move(socket))
+	session(io::ssl::context& ctx,tcp::socket&& socket) :
+		stream_(std::move(socket),ctx)
 	{
 	}
 	
@@ -43,41 +43,39 @@ public:
 		io::dispatch(stream_.get_executor(), 
 			[self = shared_from_this()]() 
 			{
-				self->read_client();
+				self->perform_ssl_handshake();
 			});
 		/*read_client();*/
 	}
 	void close() { 
 		beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
-		// stream_.shutdown([self=shared_from_this()](error_code ec) 
-		// 	{
-		// 		if (ec == boost::asio::ssl::error::stream_truncated)
-		// 		{
-		// 			return;
-		// 		}
-		// 		std::cout << ec.message() << std::endl;
-		// 	});
-		stream_.close();
-		return ;
+		stream_.async_shutdown([self=shared_from_this()](error_code ec) 
+			{
+				if (ec == boost::asio::ssl::error::stream_truncated)
+				{
+					return;
+				}
+				std::cout << ec.message() << std::endl;
+			});
 	};
 private:
 
-	// void perform_ssl_handshake()
-	// {
-	// 	beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
-	// 	stream_.async_handshake(ssl::stream_base::server, 
-	// 			[self=shared_from_this()](error_code ec) 
-	// 			{
-	// 			self->ec_ = ec;
-	// 			if (ec) {
-	// 				//std::cout << "error handshake\n";
-	// 			}
-	// 			else {
-	// 				// self->read_client();
-	// 			}
-	// 		});
+	void perform_ssl_handshake()
+	{
+		beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+		stream_.async_handshake(ssl::stream_base::server, 
+				[self=shared_from_this()](error_code ec) 
+				{
+				self->ec_ = ec;
+				if (ec) {
+					//std::cout << "error handshake\n";
+				}
+				else {
+					self->read_client();
+				}
+			});
 
-	// }
+	}
 
 	void read_client()
 	{
@@ -93,6 +91,7 @@ private:
 			}
 
 			self->handle_read();
+			//self->write_client();
 		});
 	}
 		catch (const boost::system::system_error& ec)
@@ -135,6 +134,7 @@ private:
 		default:
 			break;
 		}
+		//write_client();
 	}
 
 
@@ -155,7 +155,7 @@ private:
 
 	}
 
-	http::response<http::string_body> handle_bad_request()
+	http::response<http::string_body>&& handle_bad_request()
 	{
 		http::response<http::string_body> response{ http::status::bad_request,
 			request_.version() };
@@ -197,10 +197,10 @@ private:
 		}
 		else {
 
-			// path.append(item.data(), item.size());
-			// for (auto& c : path)
-			// 	if (c == '/')
-			// 		c = '\\';
+			path.append(item.data(), item.size());
+			for (auto& c : path)
+				if (c == '/')
+					c = '\\';
 			if (path.back() == '\\')
 			{
 				path = "index.html";
@@ -270,25 +270,18 @@ private:
 		
 		std::ofstream os(filename, std::ios::out);
 		os << str;
-		os.flush();
 		os.close();
 
 		std::FILE* fp;
+		/*system("cd");
+		system("dir");*/
+		system("g++ -std=c++17 ./code.cpp -o code.exe");
+		fp = _popen("code.exe ","rt");
+
 		std::string result{};
-
-		// system("pwd && ls");
-		// system("./code");
-		fp = popen("g++ -std=c++17 code.cpp -o code && ./code","r");
-		
-		if(fp==NULL)
-		{
-			std::cout<<"fp NULLED\n";
-		}else{
-
-			char tempbuf_[128];
-			while (fgets(tempbuf_, 128, fp)) {
-				result += tempbuf_;
-			}
+		char tempbuf_[128];
+		while (fgets(tempbuf_, 128, fp)) {
+			result += tempbuf_;
 		}
 		//std::cout << result << std::endl;
 		
@@ -322,11 +315,49 @@ private:
 		http::write(stream_,std::move(res_), ec_);
 	}
 
+
+
+	void write_client()
+	{
+	
+		try {
+			std::cout << "\nwriting\n";
+			http::string_body::value_type body;
+			body.append("<html><body>hello world<a href=""https://www.w3schools.com"">Visit W3Schools</a>\
+								<input type=""text"" id=""name:"" name=""name"" required\
+								minlength = ""4"" maxlength = ""8"" size = ""10"" > </body></html>");
+			auto const size = body.size();
+			http::response<http::string_body> res_{
+			std::piecewise_construct,
+			std::make_tuple(std::move(body)),
+			std::make_tuple(http::status::ok, request_.version()) };
+			res_.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+			res_.set(http::field::content_type, "text / html");
+			res_.content_length(size);
+			res_.keep_alive(request_.keep_alive());
+			
+			http::write(stream_, res_, ec_);
+			/*http::async_write(stream_, res_, [self=shared_from_this()](error_code ec,std::size_t bytes_transferred) {
+					
+					self -> ec = ec;
+					self->read_client();
+				});*/
+		}
+		catch (const boost::system::system_error& ec)
+		{
+			std::cout << "\nstart_accept: " << ec.what();
+		}
+		catch (const std::exception& e)
+		{
+			
+			std::cout << "\nsfskladf " << e.what();
+		}
+	}
+	
 private:
 	beast::flat_buffer flatbuf_;
 	boost::beast::error_code ec_;
-	// beast::ssl_stream<beast::tcp_stream>stream_;
-	beast::tcp_stream stream_;
+	beast::ssl_stream<beast::tcp_stream>stream_;
 	http::request<http::string_body> request_;
 	std::string error_msg;
 };
@@ -334,11 +365,10 @@ private:
 class server :public  std::enable_shared_from_this<server>
 {
 public:
-	server(io::io_context& io_context,short unsigned port)
+	server(io::io_context& io_context)
 		: io_context_(io_context),
-		acceptor_(io_context, tcp::endpoint{ io::ip::make_address("0.0.0.0"),port })
+		acceptor_(io_context, tcp::endpoint{ io::ip::make_address("127.0.0.1"),8080 })
 	{
-		std::cout<<"server constructor\n";
 		error_code ec;
 		acceptor_.listen(io::socket_base::max_listen_connections, ec);
 		check("error listenning\n")
@@ -346,19 +376,22 @@ public:
 	}
 	void start()
 	{
-		// load_server_certificate(ctx_);
+		load_server_certificate(ctx_);
 		start_accept();
 	}
 	
 	void start_accept()
 	{
-		
+		// The new connection gets its own strand
+		/*socket.emplace(io::make_strand(io_context_));*/
+		//std::shared_ptr<tcp::socket> socket(new tcp::socket(io::make_strand(io_context_)));
 		acceptor_.async_accept(
 			[self=shared_from_this()](error_code ec,tcp::socket&& socket) {
 			check("error accepting connection\n")
+			//std::cout << "\nnew connection\n";
 			try
 			{
-				std::make_shared<session>(std::move((socket)))->serve_client(); 
+				std::make_shared<session>(self->ctx_, std::move((socket)))->serve_client(); 
 				self->start_accept();
 			}
 			catch (const error_code &ec)
@@ -370,27 +403,18 @@ public:
 
 private:
 	io::io_context& io_context_;
-	// ssl::context ctx_{ ssl::context::tlsv13 };
+	ssl::context ctx_{ ssl::context::tlsv13 };
 	tcp::acceptor acceptor_;
 	tcp::endpoint endpoint_{ io::ip::make_address("127.0.0.1"),8080 };
+	//std::optional<tcp::socket> socket;
 };
 
 int main(int argc, char* argv[])
 {
-	std::cout<<"Welcome \n";
 	try
 	{
-		char* port_p=std::getenv("PORT");
-		short unsigned port;
-		if(port_p){
-				port=static_cast<unsigned short>(std::atoi(port_p));
-		}else
-		{
-			port=8080;
-		}
-
 		io::io_context io_context;
-		std::make_shared<server>(io_context,port)->start();
+		std::make_shared<server>(io_context)->start();
 		io_context.run();
 		return 0;
 	}
@@ -398,5 +422,4 @@ int main(int argc, char* argv[])
 	{
 		std::cout<<"what : " << e.message();
 	}
-	
 }
